a65fd45dd8573cc65067819b312ddf2c8b05521f
naimsadiq <nayemsadiq2013@gmail.com>
2025-08-27 22:48:12 +0600

    calling button function crate done

--- javascript/script.js
+++ javascript/script.js
@@ -31,3 +31,33 @@ for(const btn of heartBtns){
 })
 };
 
+// calling btn function
+const callingCoin = 20;
+const callBtns = getClass('call-btn');
+for(const callBtn of callBtns){
+    callBtn.addEventListener('click', function(){
+        const abalableCoin = getIdNumber('coin');
+        if(abalableCoin < callingCoin){
+            alert('আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।');
+            return;
+        }
+        const totalCoin = abalableCoin - callingCoin;
+        getId('coin').innerText = totalCoin;
+        const cardConteiner = getId('history-conteinar');
+        const title = callBtn.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+        const subtitle = callBtn.parentNode.parentNode.childNodes[3].childNodes[3].innerText;
+        const callingNumber = callBtn.parentNode.parentNode.childNodes[3].childNodes[5].childNodes[1].innerText;
+        let timeShow = new Date().toLocaleTimeString();
+        alert(`Caliing ${subtitle} Service ${callingNumber}...`)
+        const newElement = document.createElement('div');
+        newElement.innerHTML = `<div class="hidtory-cart p-4 mt-2 bg-[#FAFAFA] rounded-lg flex justify-between items-center">
+                                        <div>
+                                            <h2 class="text-lg text-[#111111]">${title}</h2>
+                                            <span class="text-lg text-[#5C5C5C]">${callingNumber}</span> 
+                                        </div>
+                                        <span class="text-lg text-[#111111]">${timeShow}</span>
+                                    </div>`
+        cardConteiner.appendChild(newElement);
+    })
+}
+
